6a77cfbac370c7c7d5fd2907aded7f7fb1edca6e
e1000son <emilsonvontade@gmail.com>
2023-11-12 21:07:07 +0200

    Envio de notificação na alocação de um processo

--- src/main/java/mz/gov/inagegpjnotificationservice/service/SendEmail.java
+++ src/main/java/mz/gov/inagegpjnotificationservice/service/SendEmail.java
@@ -1,14 +1,33 @@
 package mz.gov.inagegpjnotificationservice.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import mz.gov.inagegpjnotificationservice.entities.Notification;
 import mz.gov.inagegpjnotificationservice.entities.enums.SendMeanType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SendEmail implements ISendEngine{
+
+    @Autowired
+    private JavaMailSender sender;
     @Override
     public String sendMessage(Notification notification) {
-        return "Enviando EMAIL...";
+        MimeMessage message = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        try {
+            helper.setTo(notification.getDestiny());
+            helper.setSubject("Subject: Notificacao");
+            helper.setText(notification.getMessageText());
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+        sender.send(message);
+        return "Notoficação enviada";
     }
 
     @Override
